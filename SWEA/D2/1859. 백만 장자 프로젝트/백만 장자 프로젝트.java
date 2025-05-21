af89a6f4f8651fc4a24a59d441086aa4ba426a6e
Seongwon <53917860+codearchitect99@users.noreply.github.com>
2025-05-21 19:06:32 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 2,047 ms, Memory: 131,456 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,27 +1,29 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		int T;
-		T=Integer.parseInt(br.readLine());
-        	
+		T=sc.nextInt();
+
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            int N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] nums = new int[N];
-            for (int i = 0; i < N; i ++) {
-                nums[i] = Integer.parseInt(st.nextToken());
+            int N = sc.nextInt();
+            int[] nums = new int[N];
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = sc.nextInt();
             }
             
             int max = nums[N - 1];
             long result = 0;
             for (int i = N - 2; i >=0; i--) {
             	if (max < nums[i]) max = nums[i];
-                else result += max - nums[i];
+                else {
+                    result += max - nums[i];
+                }
             }
             System.out.printf("#%d %d", test_case, result);
             System.out.println();
