d678ac521b4cc0a8725c7fc097562799876c2eee
Seongwon Yang <53917860+codearchitect99@users.noreply.github.com>
2026-01-29 23:42:17 +0900

    [Gold III] Title: 줄 세우기, Time: 516 ms, Memory: 53504 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -10,19 +10,16 @@ public class Main {
     private static int m;
     private static ArrayList<Integer>[] edges;
     private static int[] counts;
-    private static boolean[] used;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
-        used = new boolean[n + 1];
         edges = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             edges[i] = new ArrayList<>();
         }
-        counts = new int[n + 1];    // idx 학생보다 앞에 최소 몇명이 있어야 하는지
-
+        counts = new int[n + 1];
         for (int i = 0; i < m; i++) {
             input = br.readLine().split(" ");
             int front = Integer.parseInt(input[0]);
@@ -31,27 +28,20 @@ public class Main {
             counts[back]++;
         }
 
-        sort(0);    // 맨 앞 0 번째 학생 세우기
-
-        System.out.println(sb);
-    }
-
-    private static void sort(int idx) {
-        if (q.isEmpty()) {
-            for (int i = 1; i <= n; i++) {
-                if (counts[i] == 0 && used[i] == false) {
-                    q.offer(i);
-                }
+        for (int i = 1; i <= n; i++) {
+            if (counts[i] == 0) {
+                q.offer(i);
             }
         }
-        if (q.isEmpty()) return;
-        int current = q.poll();
-        used[current] = true;
-        sb.append(current).append(" ");
-        for (int i : edges[current]) {
-            counts[i]--;
-            if (counts[i] == 0 && used[i] == false) q.offer(i);
+        while (!q.isEmpty()) {
+            int current = q.poll();
+            sb.append(current).append(" ");
+            for (int i : edges[current]) {
+                counts[i]--;
+                if (counts[i] == 0) q.offer(i);
+            }
         }
-        sort(idx + 1);
+
+        System.out.println(sb);
     }
 }
